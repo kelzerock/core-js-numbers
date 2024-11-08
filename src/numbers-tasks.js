0aba90a8d54b95363afd7dcea8046f2e93f816f2
Aleksei Zhuchkov <kelzzzer@gmail.com>
2024-11-08 11:09:44 +0300

    feat: complete getFibonacciNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -253,8 +253,13 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  const fibonacciArr = [0, 1];
+  if (index < 2) return fibonacciArr[index];
+  for (let i = 1; i <= index; i += 1) {
+    fibonacciArr.push(fibonacciArr[i] + fibonacciArr[i - 1]);
+  }
+  return fibonacciArr[index];
 }
 
 /**
